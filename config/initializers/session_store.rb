00d90df986b0ead537b14a4d5b983684d4bac288
Platon <platonny@ngs.ru>
2016-02-04 14:28:12 +0600

    increase user session timeout

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Fractalator::Application.config.session_store :cookie_store, key: '_fractalator_session'
+Fractalator::Application.config.session_store :cookie_store, key: '_fractalator_session', expire_after: 6.months
